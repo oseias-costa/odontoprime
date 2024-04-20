90fb656dd99b700c83f8d2ad5b1cd62647917475
Oséias Costa <oseiasc2@gmail.com>
2024-04-20 10:42:25 -0300

    fix: remove second pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       <body>
         <StyledComponentsRegistry>
           {children}
-          <FacebookPixel />
+          {/* <FacebookPixel /> */}
         </StyledComponentsRegistry>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,17 @@ import Clinic from "./components/clinic/Clinic";
 import Maps from "./components/maps/Maps";
 import Footer from "./components/footer/Footer";
 import Whatsapp from "./components/whatsapp/Whatsapp";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
-  // useEffect(() => {
-  //   import('react-facebook-pixel')
-  //     .then((x) => x.default)
-  //     .then((ReactPixel) => {
-  //       ReactPixel.init('1809081562889574') // facebookPixelId
-  //       ReactPixel.pageView()
-  //     })
-  // }, [])
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('1809081562889574') // facebookPixelId
+        ReactPixel.pageView()
+      })
+  }, [])
 
   return (
     <main>
